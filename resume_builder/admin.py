1c5724ea62a8fe8909577c9ba05fb9f30a639753
Owais <121290492+owaissaee@users.noreply.github.com>
2025-06-29 14:17:26 +0500

    Add files via upload

--- resume_builder/admin.py
+++ resume_builder/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (
+    ResumeTemplate,
+    Resume,
+    ResumeSection,
+    WorkExperience,
+    TechnicalSkill,
+    Education,
+    Technology,
+    Project,
+    Certification,
+    Award,
+    Language,
+)
 
 # Register your models here.
+admin.site.register(ResumeTemplate)
+admin.site.register(Resume)
+admin.site.register(ResumeSection)
+admin.site.register(WorkExperience)
+admin.site.register(TechnicalSkill)
+admin.site.register(Education)
+admin.site.register(Technology)
+admin.site.register(Project)
+admin.site.register(Certification)
+admin.site.register(Award)
+admin.site.register(Language)
